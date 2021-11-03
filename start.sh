254d59bb8ec50eb9c2cfeb2b74cde09802318506
mary-wan <mary.njenga@student.moringaschool.com>
2021-11-03 14:18:33 +0300

    add credentials

---
+++ start.sh
@@ -0,0 +1,5 @@
+export MOVIE_API_KEY=e115bf885009619f6711436d45c9348a
+export SECRET_KEY=45678904567890
+export MAIL_USERNAME=tylame990@gmail.com
+export MAIL_PASSWORD=Wanjiku123.
+python3.6 manage.py server
