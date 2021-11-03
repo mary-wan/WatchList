48aa046fc7dbd0fd7dfd90e72667517e0ef91e39
mary-wan <mary.njenga@student.moringaschool.com>
2021-11-03 13:26:39 +0300

    add upload pic functionality

--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,7 @@ from ..request import get_movies,get_movie,search_movie
 from .forms import ReviewForm,UpdateProfile
 from ..models import Review,User
 from flask_login import login_required
-from .. import db
+from .. import db,photos
 
 # Views
 @main.route('/')
@@ -87,11 +87,21 @@ def update_profile(uname):
     form = UpdateProfile()
 
     if form.validate_on_submit():
-        user.bio = form.bio.data
-
+        user.bio = form.bio.data  
         db.session.add(user)
         db.session.commit()
 
         return redirect(url_for('.profile',uname=user.username))
 
     return render_template('profile/update.html',form =form)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
